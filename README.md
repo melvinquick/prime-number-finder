fa8484c354e8dc8c41bc2d2d21c5268678d6761a
Charlie Quick <cquickreal00@gmail.com>
2022-06-10 11:36:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Python Prime Number Finder
+
+## **Purpose**
+
+The purpose of this project is to make a simple app to look for Prime Numbers.
+
+## **Capabilities**
+
+- Finds primes indefinitely (it runs until you kill it with a keyboard interrupt [ctrl+c] on Windows while in terminal window)
+- Gets rid of Semiprimes
+- Gets rid of Squarefree Primes
+- Stores found Primes in a text file called "prime_numbers.txt" (this file is pre-made and starts as empty as I didn't include code to create it)
+- Stores the number that you were on before terminating the program in a file called "current_number.txt" (this file is pre-made and starts with just 2 as the value [to get the program off and running] as I didn't include code to create it)
+    - This allows for starting back up where you left off and/or putting your own starting number in the file and starting from wherever YOU want to
+
+## **Notes**
+- I made this for fun in my spare time to have a crack at finding the next elusive Prime Number!
---
+++ prime_number_finder.py
@@ -0,0 +1,163 @@
+# Rules for finding Prime Numbers
+## Aside from 2, no even number is Prime
+## If the sum of the digits is divisible by 3, then the number will be divisble by 3
+## If the number end with 0 or 5, then it will be divisible by 5
+## Double the last digit and subtract it from the rest of the number... If the answer is divisible by 7, the original number will be divisible by 7... For instance, if the number is 161, take the last digit (1), double it, then subtract it from the rest of the number (16)... If the answer is divisible by 7, then so is the original number... In this case the answer is 14, 14 is divisible by 7, so 161 is also.
+## Add alternate digits and subtract it from the difference of the next sum of alternate digits... For instance, if the number is 574652, add 5+4+5=14 and 7+6+2=15... If the difference i.e. 1 is divisible by 11, then the number will be divisible by 11... In this case, clearly the number is not divisible by 11.
+
+
+# Function to determine the sum of the digits of the number
+def digit_sum_func(number):
+    ## Local Variables
+    digit_sum = 0
+    digits = str(number)
+
+    ## Local Main Code
+    for digit in digits:
+        digit_sum += int(digit)
+    return digit_sum
+
+
+# Function to check last digit of the number
+def last_digit_func(number):
+    ## Local Variables / Local Main Code
+    last_digit = int(str(number)[-1:])
+    return last_digit
+
+
+# Function to check for the divisible by 7 condition
+def seven_check_func(number):
+    ## Local Variables / Local Main Code
+    last_digit = int(str(number)[-1:])
+    new_number_1 = int(str(number)[:-1])
+    new_number_2 = new_number_1 - (2 * last_digit)
+    return new_number_2
+
+
+# Function to check for the divisible by 11 condition
+def eleven_check_func(number):
+    ## Local Variables / Local Main Code
+    new_number_1 = int(str(number)[0::2])
+    new_number_1 = digit_sum_func(new_number_1)
+    new_number_2 = int(str(number)[1::2])
+    new_number_2 = digit_sum_func(new_number_2)
+    new_number_3 = abs(new_number_1 - new_number_2)
+    return new_number_3
+
+
+# Function to check for semiprimes
+def semiprime_check_func(number):
+    ## Local Variables / Local Main Code
+    for prime in prime_list:
+        if number == prime * prime:
+            return True
+    return False
+
+
+# Function to check for Squarefree Primes
+def squarefree_prime_check_func(number):
+    ## Local Variables
+    counter = 0
+
+    ## Local Main Code
+    for prime in prime_list:
+        counter = 0
+        while counter <= len(prime_list) - 1:
+            if number == prime * prime_list[counter]:
+                return True
+            counter += 1
+    return False
+
+
+# Funtion to determine if a number is Prime or not
+def prime_check_func(number):
+    ## Local Variables
+    digit_sum = 0
+    seven_check = 0
+    eleven_check = 0
+
+    ## Local Main Code
+    if number in starting_prime_list:
+        return True
+
+    if last_digit_func(number) in end_digit_fail_list:
+        return False
+
+    digit_sum = digit_sum_func(number)
+
+    if digit_sum % 3 == 0:
+        return False
+    
+    seven_check = seven_check_func(number)
+
+    if seven_check % 7 == 0:
+        return False
+
+    eleven_check = eleven_check_func(number)
+
+    if eleven_check % 11 == 0:
+        return False
+    
+    is_semiprime = semiprime_check_func(number)
+
+    if is_semiprime == True:
+        return False
+
+    is_squarefree_prime = squarefree_prime_check_func(number)
+
+    if is_squarefree_prime == True:
+        return False
+
+    prime_list.append(number)
+
+    return True
+
+
+# Funtion for getting the last number you checked and starting up the finder from there
+def get_startup_number_func():
+    ## Local Variables / Local Main Code
+    startup_number_file = open("current_number.txt", "r")
+    startup_number = int(startup_number_file.read())
+    startup_number_file.close()
+    return startup_number
+
+
+# Funtion for setting the last number you checked
+def set_ending_number_func(number):
+    ## Local Variables / Local Main Code
+    ending_number_file = open("current_number.txt", "w")
+    ending_number_file.write(str(number))
+    ending_number_file.close()
+    return
+
+
+# Function to store a found Prime in a file
+def store_prime_func(number):
+    ## Local Variables / Local Main Code
+    new_prime_file = open("prime_numbers.txt", "a")
+    new_prime_file.write(str(number))
+    new_prime_file.write("\n")
+    new_prime_file.close()
+    return
+
+
+# Global Variables
+starting_prime_list = [2,3,5,7,11,13,17,19,23,29]
+prime_list = [2,3,5,7,11,13,17,19,23,29]
+end_digit_fail_list = [0,2,4,5,6,8]
+current_number = get_startup_number_func()
+is_prime = False
+is_semiprime = False
+is_squarefree_prime = False
+keep_iterating = True
+
+
+# Main Code
+while keep_iterating:
+    is_prime = prime_check_func(current_number)
+
+    if is_prime == True:
+        store_prime_func(current_number)
+
+    current_number += 1
+    set_ending_number_func(current_number)
